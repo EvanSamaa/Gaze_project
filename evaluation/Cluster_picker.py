239cc0cd1e4b6aee9993117bac156f4e6bfb2b86
EvanSamaa <42326370+EvanSamaa@users.noreply.github.com>
2023-03-13 12:17:24 -0400

    changes

---
+++ evaluation/Cluster_picker.py
@@ -0,0 +1,101 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pickle as pkl
+if __name__ == "__main__":
+    # Generate some sample data
+    data = pkl.load(open("./points.pkl", "rb"))
+    # Create a scatter plot of the data
+    fig, ax = plt.subplots()
+    ax.scatter(data[:, 0], data[:, 1])
+
+    # Define a variable to store the circle patch
+    circle = None
+    output = []
+    speaker = -1
+
+    # Define a function to handle the mouse click event
+    def onclick(event):
+        # Get the x and y coordinates of the mouse click
+        x, y = event.xdata, event.ydata
+        # Check if the click is within the axes limits
+        if x is not None and y is not None:
+            # Define the initial radius of the circle
+            radius = 0.1
+
+            # Create a circle patch with the specified centroid and radius
+            global circle
+            circle = plt.Circle((x, y), radius, color='r', fill=False)
+
+            # Add the circle patch to the plot
+            ax.add_patch(circle)
+
+            # Refresh the plot
+            plt.draw()
+    # Define a function to handle the mouse move event
+    def onmove(event):
+        # Check if the left mouse button is pressed and a circle patch exists
+        if event.button == 1 and circle is not None:
+            # Get the x and y coordinates of the mouse move
+            x, y = event.xdata, event.ydata
+
+            # Calculate the distance between the mouse click and mouse move
+            radius = np.sqrt((x - circle.center[0])**2 + (y - circle.center[1])**2)
+
+            # Update the radius of the circle
+            circle.set_radius(radius)
+
+            # Refresh the plot
+            plt.draw()
+            # np.savetxt("centroid_and_radius.txt", np.array([circle.center[0], circle.center[1], radius]))
+
+    # Define a function to handle the mouse release event
+    def onrelease(event):
+        # Set the circle patch to None
+        global circle
+        global speaker
+        output.append([circle.center[0], circle.center[1], speaker])
+        radius = circle.radius
+        circle.remove()
+        if speaker == 1: # if it's a speaker
+            new_circle = plt.Circle((output[-1][0], output[-1][1]), radius, color='g', fill=False)
+        else:
+            new_circle = plt.Circle((output[-1][0], output[-1][1]), radius, color='b', fill=False)
+        circle_list.append(new_circle)
+        circle = new_circle
+        ax.add_patch(circle)
+        plt.draw()
+        circle = None
+        print("[")
+        for i in range(0, len(output)):
+            print(output[i], ", ")
+        print("]")
+    # the undo button
+    circle_list = []   
+    def on_press(event):
+        if event.key == 'z':   
+            onundo(event)
+        if event.key == "h":
+            global speaker
+            speaker = speaker * -1
+
+
+    def onundo(event):
+        if circle_list:
+            circle = circle_list.pop()
+            output.pop()
+            circle.remove()
+            plt.draw()
+            # print(output)
+
+    fig.canvas.mpl_connect('key_press_event', on_press)
+
+    # Connect the mouse click event to the onclick function
+    cid1 = fig.canvas.mpl_connect('button_press_event', onclick)
+
+    # Connect the mouse move event to the onmove function
+    cid2 = fig.canvas.mpl_connect('motion_notify_event', onmove)
+
+    # Connect the mouse release event to the onrelease function
+    cid3 = fig.canvas.mpl_connect('button_release_event', onrelease)
+    # Display the plot
+    plt.show()
--- maya_scripts/create_proxy_scene.py
+++ maya_scripts/create_proxy_scene.py
@@ -1,21 +1,27 @@
 import json
-if len(created_object_list) > 0:
+if 'created_object_list' in locals():
     for i in range(0, len(created_object_list)):
         try:
             cmds.select(created_object_list[i])
             cmds.delete()
         except:
             print("cannot find object {}".format(created_object_list[i]))
+            created_object_list = []
+else:
+    created_object_list = []
     
-file_content = json.load(open("/Users/evanpan/Documents/GitHub/Gaze_project/data/look_at_points/simplest_scene2_less_items.json"))
+file_name = "/Volumes/EVAN_DISK/MASC/Ribhav_processed_dataset/annotated_scene/Madelaine Petsch audition for The Prom.json"
+    
+file_content = json.load(open(file_name))
 # file_content = json.load(open("C:/Users/evansamaa/Documents/GitHub/Gaze_project/data/look_at_points/simplest_scene2_less_items.json"))
-print(file_content)
 temp_object_type, temp_object_pos, temp_object_interest = file_content["object_type"], file_content["object_pos"], file_content["object_interestingness"]
 for key in temp_object_type.keys():
     object_type = temp_object_type[key]
     object_pos = temp_object_pos[key]
     object_interest = temp_object_interest[key]
-    sp, _ = cmds.sphere(r = 10, pivot =object_pos)
+    sp, _ = cmds.sphere(r = 10, pivot =[0, 0, 0])
+    cmds.select(sp)
+    cmds.move(object_pos[0], object_pos[1], object_pos[2])
     created_object_list.append(sp)
     cmds.addAttr(sp, ln='object_type', at="float", k=True)
     cmds.setAttr(sp+".object_type", object_type)
