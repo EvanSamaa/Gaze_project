bfb7d53744252ece9554942f00952ae881204539
evansamaa <evan12345pan@hotmail.com>
2023-10-17 10:18:00 -0400

    e

---
+++ training/training_2023_Oct.py
@@ -0,0 +1,52 @@
+import json
+import cv2 as cv
+import pickle as pkl
+from typing import Dict, List
+import numpy as np
+from matplotlib import pyplot as plt
+import os, sys
+import librosa
+
+from scipy import stats, spatial, ndimage
+sys.path.insert(0, '/Users/evanpan/Documents/GitHub/EvansToolBox/Utils')
+sys.path.insert(0, '/Users/evanpan/Documents/GitHub/Gaze_project')
+sys.path.insert(0, "C:/Users/evansamaa/Documents/GitHub/EvansToolBox/Utils")
+sys.path.insert(0, "C:/Users/evansamaa/Documents/GitHub/Gaze_project")
+from Signal_processing_utils import intensity_from_signal, pitch_from_signal, sparse_key_smoothing, laplacian_smoothing
+from Speech_Data_util import Sentence_word_phone_parser
+from prototypes.InputDataStructures import Dietic_Conversation_Gaze_Scene_Info
+from prototypes.MVP.MVP_static_saliency_list import ObjectBasedFixSaliency
+from prototypes.MVP.MVP_Aversion_saliency_list import AversionSignalDrivenSaliency, CTSAversionSignalDrivenSaliency
+from prototypes.MVP.MVP_look_at_point_planner import HabituationBasedPlanner, RandomPlanner, PartnerHabituationPlanner
+from prototypes.MVP.MVP_eye_head_driver import HeuristicGazeMotionGenerator
+from prototypes.MVP.MVP_Aversion_saliency_list import Base_Static_Saliency_List
+from prototypes.EyeCatch.Saccade_model_with_internal_model import *
+from prototypes.Gaze_aversion_prior.Heuristic_model import *
+from prototypes.Boccignone2020.Gaze_target_planner import Scavenger_based_planner
+from prototypes.Boccignone2020.Improved_gaze_target_planner import Scavenger_planner_with_nest, Scavenger_planner_simple 
+from prototypes.JaliNeck.JaliNeck import NeckCurve
+from prototypes.Gaze_aversion_prior.Ribhav_model import predict_aversion
+from prototypes.Gaze_aversion_prior.Evan_model import Aversion111Prior
+from prototypes.InputDataStructures import AgentInfo, TurnTakingData
+from prototypes.MVP.MVP_gaze_path_planner import Responsive_planner_simple
+import pickle
+import math
+from datetime import datetime, timezone
+import scipy.stats as ss
+from Signal_processing_utils import interpolate1D
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+
+if __name__ == "__main__":
+    # inputs
+    # input_folder = "/Volumes/EVAN_DISK/MASC/Ribhav_processed_dataset/"
+    input_folder = "F:/MASC/Ribhav_processed_dataset/"
+    # input_file = "Stranger Things’ Dacre Montgomery’s Insane _Billy_ Audition Tape ｜ GQ"
+    input_file = "Madelaine Petsch audition for The Prom"
+    shot_id = 1
+    speaker_id = 1
+    turn_taking_threshold = 2
+    fps = 24
+    np.random.seed(speaker_id + shot_id)
